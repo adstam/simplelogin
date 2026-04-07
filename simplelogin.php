6566d22f1574aaa44a111d9fe40670d0d8bb5c6f
adstam <120204069+adstam@users.noreply.github.com>
2026-04-07 13:06:16 +0200

    Refactor constructor to use DispatcherInterface

--- simplelogin.php
+++ simplelogin.php
@@ -1,15 +1,14 @@
 <?php
-
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Extension\PluginInterface;
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\Event\Event;
-use Joomla\CMS\Form\Form;
+use Joomla\Event\DispatcherInterface;
 
-class PlgSystemSimplelogin extends CMSPlugin
+class PlgSystemSimplelogin extends CMSPlugin implements PluginInterface
 {
-    public function __construct(&$subject, $config)
+    public function __construct(DispatcherInterface $dispatcher, array $config)
     {
-        parent::__construct($subject, $config);
+        parent::__construct($dispatcher, $config);
     }
 }
