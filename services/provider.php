740ea4439149188e5cc68a1995ee8a43ef685334
adstam <120204069+adstam@users.noreply.github.com>
2026-04-07 13:09:23 +0200

    Add Simplelogin plugin class to provider.php

--- services/provider.php
+++ services/provider.php
@@ -1,4 +1,17 @@
 <?php
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\Event\DispatcherInterface;
+
+class PlgSystemSimplelogin extends CMSPlugin implements PluginInterface
+{
+    public function __construct(DispatcherInterface $dispatcher, array $config)
+    {
+        parent::__construct($dispatcher, $config);
+    }
+}<?php
 
 defined('_JEXEC') or die;
 
